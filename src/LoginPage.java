2dcd30a223885818e221c5f3c116b0ffa934d6f0
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-25 15:11:05 +0530

    basic GUI created

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,11 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class LoginPage implements ActionListener {
+
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
     HashMap<String, String> loginInfo = new HashMap<String, String>();
+    JLabel userIDLabel = new JLabel("UserID:");
+    JLabel userPasswordLabel = new JLabel("password:");
+    JLabel messageLabel = new JLabel();
+
+
     LoginPage(HashMap<String, String> loginInfoOriginal){
         loginInfo = loginInfoOriginal;
+
+        userIDLabel.setBounds(50,100,75,25);
+        userPasswordLabel.setBounds(50,150,75,25);
+
+        frame.add(userIDLabel);
+        frame.add(userPasswordLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 
     @Override
